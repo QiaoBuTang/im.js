703907d3fc91c1a276ff15180769579174dfa6ee
GilbertSun <szb4321@gmail.com>
2015-11-04 14:35:07 +0800

    add basic implementation

---
+++ im.js
@@ -0,0 +1,54 @@
+(function () {
+    var ORIGIN = "http://im.av.qiaobutang.com:8333";
+
+    var IM = function (businessIds) {
+        this.businessIds = businessIds.join(',');
+        this.pb = $({});
+    };
+
+    // a pub/sub pattern depend on jquery
+    IM.prototype.on = function (eventName, callback) {
+        this.pb.on.apply(this.pb, arguments);
+    };
+    IM.prototype.once = function (eventName, callback) {
+        this.pb.once.apply(this.pb, arguments);
+    };
+    IM.prototype.emit = function (eventName) {
+        this.pb.on.trigger(this.pb, arguments);
+    };
+
+    IM.prototype.open = function () {
+        $.post(ORIGIN + '/comet/web/bindAndComet', {businessIds: this.businessIds})
+            .then(function (data) {
+                var deferred = $.Deferred();
+                if (data.resultCode !== 200) {
+                    return deferred.resolve(data.uuid);
+                } else {
+                    return deferred.reject('get uuid failed');
+                }
+            })
+            .then(this.comet)
+    };
+    IM.prototype.comet = function () {
+        var _this = this;
+        return $.get(ORIGIN + '/comet/web/connect/' + this.uuid)
+            .then(function (res) {
+                if (res.resultCode === 504) {
+                    _this.comet(); // 如果超时，立刻重连
+                } else if (res.resultCode === 200) {
+                    _this.emit('message', res);
+                    _this.comet();
+                }
+            });
+    };
+    IM.prototype.close = function () {
+
+    };
+    IM.prototype.subscribe = function (channels, callback) {
+
+    };
+
+    return function (businessIds) {
+        return new IM(businessIds);
+    };
+});
