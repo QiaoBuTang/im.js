644c0c05eca5c60de3d5cb4757eb17841461cbcf
GilbertSun <szb4321@gmail.com>
2015-11-04 21:38:01 +0800

    remove un implement function

--- demo/demo.js
+++ demo/demo.js
@@ -5,6 +5,3 @@ im.open();
 im.on('message', function (evt, message) {
     console.log(message);
 });
-im.on('refuse', function () {
-    console.log('网络错误');
-});
--- im.js
+++ im.js
@@ -1,29 +1,24 @@
-var Comet = (function () {
+var IM = (function () {
     var ORIGIN = "http://im.av.qiaobutang.com:8333";
-    var MsgType = {
-        chat: 1,
-        systemMsg: 2,
-        live: 3
-    };
 
-    var IM = function (businessIds) {
+    var Comet = function (businessIds) {
         this.businessIds = businessIds;
         this.pb = $({});
         this.connect = null;
     };
 
     // a pub/sub pattern depend on jquery
-    IM.prototype.on = function (eventName, callback) {
+    Comet.prototype.on = function (eventName, callback) {
         this.pb.on.apply(this.pb, arguments);
     };
-    IM.prototype.once = function (eventName, callback) {
+    Comet.prototype.once = function (eventName, callback) {
         this.pb.once.apply(this.pb, arguments);
     };
-    IM.prototype.emit = function (eventName) {
+    Comet.prototype.emit = function (eventName) {
         this.pb.trigger.apply(this.pb, arguments);
     };
 
-    IM.prototype.open = function () {
+    Comet.prototype.open = function () {
         var _this = this;
         $.post(ORIGIN + '/comet/web/bindAndComet', {businessIds: this.businessIds})
             .then(function (data) {
@@ -44,7 +39,7 @@ var Comet = (function () {
             })
             .then($.proxy(this.comet, this))
     };
-    IM.prototype.comet = function () {
+    Comet.prototype.comet = function () {
         var _this = this;
         this.connect =  $.get(ORIGIN + '/comet/web/connect/' + this.uuid);
         this.connect
@@ -52,7 +47,7 @@ var Comet = (function () {
                 if (res.resultCode === 504) {
                     _this.comet(); // 如果超时，立刻重连
                 } else if (res.resultCode === 200) {
-                    _this.emit('message', res);
+                    _this.emit('message', res.result);
                     _this.comet();
                 }
             })
@@ -63,14 +58,11 @@ var Comet = (function () {
                 }, 20000);
             });
     };
-    IM.prototype.close = function () {
+    Comet.prototype.close = function () {
         this.connect && this.connect.abort();
-    };
-    IM.prototype.subscribe = function (channels, callback) {
-
     };
 
     return function (businessIds) {
-        return new IM(businessIds);
+        return new Comet(businessIds);
     };
 }());
