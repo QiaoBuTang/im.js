9fc9cb1e3afd9db95216112c3fd1756bcc7d3545
GilbertSun <szb4321@gmail.com>
2015-12-21 10:11:01 +0800

    update demo

--- demo/demo.js
+++ demo/demo.js
@@ -2,11 +2,11 @@ var im = IM('liveId,META_liveId');
 
 im.open();
 
-im.on('chats', function (evt, message) {
-    console.log(message);
+im.on('chats', function (evt, ret) {
+    console.log(ret.messages);
 });
-im.on('chat.liveId', function (evt, message) {
-    console.log(message);
+im.on('chat.liveId', function (evt, ret) {
+    console.log(ret.messages);
 });
 im.on('online.liveId', function (evt, res) {
     console.log(res);
